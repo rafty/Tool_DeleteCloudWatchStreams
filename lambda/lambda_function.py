ae6b2f5d88ef4eb652b4e32fc9661230b51746a7
yagitatakashi <yagita.takashi@gmail.com>
2020-03-16 23:31:38 +0900

    During refactoring

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,8 +6,8 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel(level=logging.INFO)
 logs = boto3.client('logs')
-LOGS_GROUP_NAME = '/aws/lambda/serverlessrepo-Datadog-Log-For-' \
-                  'loglambdaddfunction-1379AY0BANFA7'
+# LOGS_GROUP_NAME = '/aws/lambda/serverlessrepo-Datadog-Log-For-' \
+#                   'loglambdaddfunction-1379AY0BANFA7'
 STREAM_NAME_PREFIX = '2020/03/'
 
 
@@ -28,38 +28,46 @@ def delete_stream(stream):
     else:
         logger.info('???: {}: {}'.format(stream.get('logStreamName'), stream.get('logStreamName').startswith(STREAM_NAME_PREFIX)))
 
+# これを実装する
+# https://github.com/mihai011/Darkstorm/blob/0fe419d8c196cdc244a5b28ce50e1e6019355322/delete_log_groups.py
+
 
 def lambda_handler(event, context):
-    logger.info('event: {}'.format(event))
+    logger.info('lambda_handler(event): {}'.format(event))
 
-    response = dict()
-    response['nextToken'] = ''
+    next_token = None
+    log_groups = logs.describe_log_groups()
 
-    while True:
+    for log_group in log_groups.get('logGroups'):
+        log_group_name = log_group.get('logGroupName')
+        logger.info('delete log group: {}'.format(log_group_name))
 
-        # if response['nextToken'] == '':
-        if response.get('nextToken', '') == '':
+        # response = dict()
+        # response['nextToken'] = ''
 
-            response = logs.describe_log_streams(
-                logGroupName=LOGS_GROUP_NAME
-            )
-            logger.info(response)
+        while True:
+            # if response.get('nextToken', '') == '':
+            if next_token:
+                log_streams = logs.describe_log_streams(
+                    logGroupName=log_group_name,
+                    nextToken=next_token
+                )
+                ここから
 
-            results = list(map(lambda x: delete_stream(x), response['logStreams']))
-            logger.info('map result:{}'.format(results))
+                results = list(map(lambda x: delete_stream(x), response['logStreams']))
+                logger.info('map result:{}'.format(results))
 
-        else:
+            else:
 
-            response = logs.describe_log_streams(
-                logGroupName=LOGS_GROUP_NAME,
-                nextToken=response['nextToken']
-            )
-            logger.info(response)
+                response = logs.describe_log_streams(
+                    logGroupName=LOGS_GROUP_NAME,
+                )
+                logger.info(response)
 
-            results = list(
-                map(lambda x: delete_stream(x), response['logStreams']))
-            logger.info('map result:{}'.format(results))
+                results = list(
+                    map(lambda x: delete_stream(x), response['logStreams']))
+                logger.info('map result:{}'.format(results))
 
-        # if response['nextToken'] == '':
-        if response.get('nextToken', '') == '':
-            break
+            # if response['nextToken'] == '':
+            if response.get('nextToken', '') == '':
+                break
